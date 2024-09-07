108650710fdfb3ec64667492bb83fd24020d78ba
diamisttech <diamist@gmail.com>
2024-09-07 18:35:09 +0300

    Добавлено получение изображений по тегу, а также без тега.

--- Cataas.py
+++ Cataas.py
@@ -25,7 +25,9 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title("Картинка с котиком!")
@@ -42,9 +44,11 @@ window = Tk()
 window.title('Cats!')
 window.geometry('600x520')
 
-# Убираем кнопку и создаём меню
-# update_button = Button(text="Обновить", command=set_image)
-# update_button.pack()
+tag_entry = Entry(window)
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)

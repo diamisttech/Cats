bd24f1a3253a90ac76ecb0867bcf60c465d8766c
diamisttech <diamist@gmail.com>
2024-09-07 17:39:29 +0300

    Создана функция для получения изображения с котиком.

--- Cataas.py
+++ Cataas.py
@@ -19,8 +19,8 @@ window = Tk()
 window.title('Cats!')
 window.geometry('600x480')
 
-label1 = Label()
-label1.pack()
+label = Label()
+label.pack()
 
 url = "https://cataas.com/cat"
 img = load_image(url)

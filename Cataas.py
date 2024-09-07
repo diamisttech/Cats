c73b5c2440a00b1ef1d397d622c1ed130365bda5
diamisttech <diamist@gmail.com>
2024-09-07 17:53:52 +0300

    Добавлена кнопка для обновления изображения. Добавлено изменение размера изображения.

--- Cataas.py
+++ Cataas.py
@@ -10,23 +10,32 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         return None
 
+
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
 window = Tk()
 window.title('Cats!')
-window.geometry('600x480')
+window.geometry('600x520')
 
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = "https://cataas.com/cat"
+
+set_image()
 
 window.mainloop()

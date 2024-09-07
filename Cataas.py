52202f7fcf0c45bf25a95325439e45f8f6c1beb8
diamisttech <diamist@gmail.com>
2024-09-07 17:26:02 +0300

    Создано окно и виджеты.

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title('Cats!')
+window.geometry('600x480')
+
+label1 = Label()
+label1.pack()
+
+url = "https://cataas.com/cat"
+img = img_load(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
